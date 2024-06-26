c6eb17f3a5e32f527c5070a6b70027da596a41d0
fhrzal <fahrizaladiku@gmail.com>
2024-06-26 08:48:02 +0700

    Update marge-sort.py

--- marge-sort.py
+++ marge-sort.py
@@ -1,11 +1,13 @@
+import os
+
 def merge_sort(arr, level=0):
     if len(arr) > 1:
         mid = len(arr) // 2  # Mencari titik tengah array
         left_half = arr[:mid]  # Membagi array menjadi dua bagian
         right_half = arr[mid:]
-
-        print("Level", level, "- Membagi:", arr)
-        print("Left:", left_half, "Right:", right_half)
+        
+        print("Level", level, "- Membagi:", arr,"\n")
+        print("Kiri:", left_half, "Kanan:", right_half,"\n")
 
         merge_sort(left_half, level+1)  # Rekursi untuk bagian kiri
         merge_sort(right_half, level+1)  # Rekursi untuk bagian kanan
@@ -33,10 +35,15 @@ def merge_sort(arr, level=0):
             j += 1
             k += 1
 
-        print("Level", level, "- Menggabungkan:", arr)
+        print("Level", level, "- Menggabungkan:", arr,"\n")
+        kon = input("klik enter >>")
 
 # Contoh penggunaan
+os.system("clear")
 arr = [38, 27, 43, 3, 9, 82, 10]
-print("Array sebelum diurutkan:", arr)
+print("Array sebelum diurutkan:", arr,"\n")
+print(f"total array = ",len(arr))
+kon1 = input("klik enter >>\n")
 merge_sort(arr)
 print("Array setelah diurutkan:", arr)
+
